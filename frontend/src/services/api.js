2ab11e60fa77d70489686db60f1e25a54f5176c5
sapna maurya <io5.sapnamaurya@gmail.com>
2026-06-25 16:43:05 +0530

    changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://student-management-system-2-1zbr.onrender.com/api',
   withCredentials: true,
 });
 
